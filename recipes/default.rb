61697f4a7fdc2ae28adaf4b5b00c26a8407fd267
Steve Nolen <technolengy@gmail.com>
2014-12-18 11:30:34 -0800

    include base recipesd

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,28 @@
 # Cookbook Name:: cens-base
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
+# Author: Steve Nolen <technolengy@gmail.com>
 #
-# All rights reserved - Do Not Redistribute
+# Copyright (c) 2014.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+node.set['system']['packages']['uninstall']['nano']
+node.set['system']["timezone"] = "US/Pacific"
+
+include_recipe "system::hostname"
+include_recipe "system::timezone"
+include_recipe "system::uinstall_packages"
+include_recipe "vim"
+include_recipe "emacs"
+include_recipe "ntp"
