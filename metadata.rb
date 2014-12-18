86411039d24a04850b137d3a03304e252f490132
Steve Nolen <technolengy@gmail.com>
2014-12-18 12:04:55 -0800

    oops need metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'All rights reserved'
 description      'Installs/Configures cens-base'
 long_description 'Installs/Configures cens-base'
 version          '0.1.0'
+
+depends "system", "~> 0.3.3"
+depends "vim", "~> 1.1.2"
+depends "emacs", "~> 0.9.0"
+depends "ntp", "~> 1.6.5"
