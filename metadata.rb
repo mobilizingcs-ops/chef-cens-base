e1a766eed70e1ec9a4fc27f1722a7fa5dad47f27
Steve Nolen <technolengy@gmail.com>
2015-02-09 15:02:35 -0800

    allow password auth for now.

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures cens-base'
 long_description 'Installs/Configures cens-base'
-version          '0.1.12'
+version          '0.1.17'
 
 depends "system", "~> 0.3.3"
 depends "vim", "~> 1.1.2"
 depends "emacs", "~> 0.9.0"
 depends "ntp", "~> 1.6.5"
 depends 'openssh', '~> 1.3.4'
-depends 'nfs'
+depends 'nfs', '~> 2.0.2'
 depends 'curl', '~> 2.0.1'
 depends 'postfix', '~> 3.6.2'
 depends 'apt', '~> 2.6.1'
