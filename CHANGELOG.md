3de9e361d1b467c233f04b86e91429106e61e277
Steve Nolen <technolengy@gmail.com>
2015-02-10 09:49:06 -0800

    version to 1.0, this is going into production

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
-# 0.2.0
-chef-client will automatically check in
+# 1.0.0
+chef-client will automatically check in, should be very stable on all linux nodes in this environment
 
 # 0.1.10
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures cens-base'
 long_description 'Installs/Configures cens-base'
-version          '0.2.1'
+version          '1.0.0'
 
 depends "system", "~> 0.3.3"
 depends "vim", "~> 1.1.2"
