53bed8a23f716f331873063ae9e8e6fcede0070a
Steve Nolen <technolengy@gmail.com>
2015-12-08 13:12:47 -0800

    pin to supermarket nfs instead of fork. now supports freebsd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-base'
 long_description 'Installs/Configures cens-base'
-version '1.2.1'
+version '1.2.2'
 
 %w(ubuntu debian freebsd centos).each do |os|
   supports os
@@ -14,7 +14,7 @@ depends 'vim', '~> 1.1.2'
 depends 'emacs', '~> 0.9.0'
 depends 'ntp', '~> 1.6.5'
 depends 'openssh', '~> 1.3.4'
-depends 'nfs', '~> 2.0.2'
+depends 'nfs', '~> 2.2.6'
 depends 'curl', '~> 2.0.1'
 depends 'postfix', '~> 3.6.2'
 depends 'apt', '~> 2.6.1'
